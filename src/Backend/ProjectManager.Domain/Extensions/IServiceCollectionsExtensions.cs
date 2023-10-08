65dcc7d09cc953d0fcf7c955fc0348ee291396af
Dmitry Shashev <DmitryShashev@gmail.com>
2023-10-08 23:21:23 +0700

    Added extesions to servises collections

---
+++ src/Backend/ProjectManager.Domain/Extensions/IServiceCollectionsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Domain.Abstractions.Services;
+using ProjectManager.Domain.Context;
+using ProjectManager.Domain.Services;
+
+namespace ProjectManager.Domain.Extensions
+{
+    public static class IServiceCollectionsExtensions
+    {
+        public static IServiceCollection AddProjectManager(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            services.AddDbContext<ApplicationContext>(options);
+
+            services.AddScoped<IBoardService, BoardService>();
+            services.AddScoped<ITaskService, TaskService>();
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
